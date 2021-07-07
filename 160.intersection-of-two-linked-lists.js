4f2942761069e8627ff710a4f40c1b644e0f31a1
Matthias Maisch <matthias@maisch.me>
2021-07-07 21:05:58 +0200

    intersection of two linked lists first try 😲

---
+++ 160.intersection-of-two-linked-lists.js
@@ -0,0 +1,54 @@
+/*
+ * @lc app=leetcode id=160 lang=javascript
+ *
+ * [160] Intersection of Two Linked Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @typedef ListNode
+ * @property {number} val
+ * @property {ListNode} next
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    let setA = new Set();
+    let currentHeadA = headA;
+    while (currentHeadA) {
+        if (setA.has(currentHeadA)) {
+            break;
+        }
+        setA.add(currentHeadA);
+        currentHeadA = currentHeadA.next;
+    }
+
+    let setB = new Set();
+    let currentHeadB = headB;
+    while (currentHeadB) {
+        if (setB.has(currentHeadB)) {
+            break;
+        }
+        if (setA.has(currentHeadB)) {
+            return currentHeadB;
+        }        
+        setB.add(currentHeadB);
+        currentHeadB = currentHeadB.next;
+    }
+
+    return null;    
+};
+// @lc code=end
+
