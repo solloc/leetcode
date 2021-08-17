e7150408edb61bd20dac9b31984619da6c4ab25e
solloc <matthias@maisch.me>
2021-08-17 19:19:31 +0200

    remove linked list element OK ✔

--- 203.remove-linked-list-elements.js
+++ 203.remove-linked-list-elements.js
@@ -12,12 +12,38 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
 /**
  * @param {ListNode} head
  * @param {number} val
  * @return {ListNode}
  */
 var removeElements = function(head, val) {
+    if (!head) {
+        return head;
+    }
+
+    let current = head;
+    while (current && current.next) {
+        if (current.next.val === val) {
+            current.next = current.next.next;
+            continue;
+        }
+        current = current.next;
+    }
+    if (head && head.val === val) {
+        head = head.next;
+    }
+
+    if (head && head.val === val) {
+        head = null;
+    }
+
+    return head;
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = removeElements;
+// @after-stub-for-debug-end
