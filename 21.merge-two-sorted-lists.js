52d18fc0d00626cc7170d7a682c38a090b0993c2
solloc <matthias@maisch.me>
2021-08-30 20:17:18 +0200

    merge two sorted lists ✔

--- 21.merge-two-sorted-lists.js
+++ 21.merge-two-sorted-lists.js
@@ -12,13 +12,59 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+/**
+ * @typedef ListNode
+ * @property {number} val
+ * @property {ListNode} next
+ */
+
 /**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    
+    if (!l2) {
+        return l1;
+    }
+    if (!l1) {
+        return l2;
+    }
+    let primary = l1;
+    let secondary = l2;
+    if (l2.val < l1.val) {
+        primary = l2;
+        secondary = l1;
+    }
+
+    let currentPrimary = primary;
+    let currentSecondary = secondary;
+    let tail = null;
+    while (currentPrimary) {
+        while (currentSecondary) {
+            if (currentPrimary.next && currentSecondary.val < currentPrimary.next.val) {
+                let newPrimary = currentSecondary;
+                currentSecondary = currentSecondary.next;
+                newPrimary.next = currentPrimary.next;
+                currentPrimary.next = newPrimary;
+            } else {
+                break;
+            }
+        }        
+        if (!currentPrimary.next) {
+            tail = currentPrimary;
+        }
+        currentPrimary = currentPrimary.next;
+    }
+
+    tail.next = currentSecondary;
+
+    return primary;
 };
 // @lc code=end
 
+
+// @after-stub-for-debug-begin
+module.exports = mergeTwoLists;
+// @after-stub-for-debug-end
